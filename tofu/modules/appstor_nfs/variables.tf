4e0daf5831ceb7020aa0a2e6d0cd6cc17df2a5a0
Robert Ayrapetyan <robert@yag.im>
2026-03-29 03:35:11 +0000

    del: appstor_nfs module

--- tofu/modules/appstor_nfs/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "servers" {
-  type = list(object({
-    host       = string
-    nfs_port   = optional(number, 2049)
-    ovh_region = optional(string, "N/A")
-    region     = string
-    ssh_port   = optional(number, 22)
-  }))
-}
